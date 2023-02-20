9a9aa6e37aa8ea237509f1eb78df7b72bdf9b8f7
m-gibas <gibasmaksymilian@gmail.com>
2023-02-20 17:40:40 +0100

    navbar styling update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,65 +1,51 @@
-import { useRef } from 'react';
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { BsFillPersonFill, BsFillHouseFill } from 'react-icons/bs'
 import { AiOutlineLink } from 'react-icons/ai'
-import { CSSTransition } from 'react-transition-group'
 import './Navbar.css'
 
 
-
 const Navbar = () => {
-  const location = useLocation()
-  const nodeRef = useRef(null);
 
   return (
     <div className="navbar">
-      {/* <CSSTransition 
-              timeout={300}
-              classNames={"navbar-transition"}
-              unmountOnExit
-              nodeRef={nodeRef}
-              > */}
-
-        <NavLink ref={nodeRef} to="/" 
+        <NavLink to="/" 
           className={({ isActive }) =>
           isActive ? "navbar-link navbar-active" : "navbar-link"}>
              {({ isActive }) => (
               <>
-                <p className={isActive ? 'navbar-title' : 'hide-element'}>
+                <p className={isActive ? 'navbar-title' : 'hide-navbar-title'}>
                   Home
                 </p>
-                <BsFillHouseFill className={isActive ? 'hide-element' : 'icon' } size={'2rem'} /> 
+                <BsFillHouseFill className={isActive ? 'move-icon' : 'icon' } size={'2rem'} /> 
               </>
             )}
         </NavLink>
-      {/* </CSSTransition> */}
 
         <NavLink to="/about" 
           className={({ isActive }) =>
           isActive ? "navbar-link navbar-active" : "navbar-link"}>
              {({ isActive }) => (
               <>
-                <p className={isActive ? 'navbar-title' : 'hide-element'}>
+                <p className={isActive ? 'navbar-title' : 'hide-navbar-title'}>
                   About
                 </p>
-                <BsFillPersonFill className={isActive ? 'hide-element' : 'icon' } size={'2rem'} /> 
+                <BsFillPersonFill className={isActive ? 'move-icon' : 'icon' } size={'2rem'} /> 
               </>
             )}
         </NavLink>
+
         <NavLink to="/links" 
           className={({ isActive }) =>
           isActive ? "navbar-link navbar-active" : "navbar-link"}>
              {({ isActive }) => (
               <>
-                <p className={isActive ? 'navbar-title' : 'hide-element'}>
+                <p className={isActive ? 'navbar-title' : 'hide-navbar-title'}>
                   Links
                 </p>
-                <AiOutlineLink className={isActive ? 'hide-element' : 'icon' } size={'2rem'} /> 
+                <AiOutlineLink className={isActive ? 'move-icon' : 'icon' } size={'2rem'} /> 
               </>
             )}
         </NavLink>
-
-
       </div>
   )
 }
