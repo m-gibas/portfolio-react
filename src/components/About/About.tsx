41dacc2c1893eb53d397d8497713b2e98d1dc03f
Maksymilian Gibas <104219943+m-gibas@users.noreply.github.com>
2023-02-26 17:10:39 +0100

    added links hrefs

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,17 @@
 import './About.css'
+import about from './about.jpg'
 
 const About = () => {
   return (
     <div className="container container-about">
-        <p>About page</p>
+        <section>
+          <h3>Hi!</h3> 
+          <span>
+          My name is Max and I'm currently seeking a job! <br />
+          At the moment I'm trying to learn React with TypeScript.
+          </span>
+        </section>
+        <img src={about} alt='about' />
     </div>
   )
 }
--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -1,9 +1,28 @@
 import './Links.css'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
 
 const Links = () => {
   return (
     <div className="container container-links">
-        <p>You can find me there:</p>
+        <h3>You can find me there:</h3>
+        <div className='links-icons'>
+          <a
+              href="https://github.com/m-gibas"
+              target="_blank"
+              rel="noreferrer noopener"
+              title="Click me!"
+              className='links-icon'
+            ><BsGithub size={'2.5rem'} />
+          </a>
+          <a
+              href="https://www.linkedin.com/in/maksymilian-gibas-259791235/"
+              target="_blank"
+              rel="noreferrer noopener"
+              title="Click me!"
+              className='links-icon'
+            ><BsLinkedin size={'2.5rem'} />
+          </a>
+        </div>
     </div>
   )
 }
